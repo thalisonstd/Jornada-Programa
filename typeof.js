da04fad5895968d34e2d1e56a01815b177d46a9d
Thalison <thalisonbq@gmail.com>
2026-05-17 17:52:40 -0300

    tipo de variavel

---
+++ typeof.js
@@ -0,0 +1,10 @@
+const nome = "Lucas"
+const idade = 18
+const aprovado = true
+
+console.log(nome)
+console.log(typeof nome)
+console.log(idade)
+console.log(typeof idade)
+console.log(aprovado)
+console.log(typeof aprovado)
