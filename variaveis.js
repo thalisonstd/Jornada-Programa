00e0cb1dfaba72c196a0e565cd0c9b2f6fb8bd01
Thalison <thalisonbq@gmail.com>
2026-05-17 17:15:15 -0300

    adição de exibição

--- variaveis.js
+++ variaveis.js
@@ -2,3 +2,8 @@ const nome = "Thalison"
 let idade = 18
 const cidade = "Rio de Janeiro"
 const sonho = "Programador"
+
+console.log(nome)
+console.log(idade)
+console.log(cidade)
+console.log(sonho)
