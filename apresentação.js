a1d4337f1e439cf62dc7eb47844a42b6cd7c2f5f
Thalison <thalisonbq@gmail.com>
2026-05-17 16:17:27 -0300

    primeiro commit

---
+++ apresentação.js
@@ -0,0 +1,4 @@
+console.log("Nome: Thalison")
+console.log("Idade: 23")
+console.log("Objetivo: Aprender desenvolvimento fullstack")
+console.log("Frase motivacional: Um verdadeiro mestre é um eterno aprendiz")
